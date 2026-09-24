1ac13c55fa4332f42ba5d8ce66b2d7c7b6bc5616
Qwertyzx1c <jakub.ura10@gmail.com>
2026-09-24 09:33:57 +0200

    Create projekt_task_manager.py

---
+++ projekt_task_manager.py
@@ -0,0 +1,21 @@
+
+slownik = {
+    "nazwa_zadania": "sperma",
+    "przypisana_osoba": "ty",
+    "priorytet" : "wysoki",
+    "status" : "w trakcie",
+    "kategorie" : "backend"
+}
+
+while(True):
+	print("---------------------------------------")
+	print("> System Zarządzania Zadaniami Zespołu |")
+	print("> Wybierz opcje                        |")
+	print("1) Dodaj zadanie                       |")
+	print("2) Wyświetl wszystkie zadania          |")
+	print("3) Wyszukaj zadanie                    |")
+	print("4) Oznacz jako wykonane                |")
+	print("5) Usuń zadanie                        |")
+	print("6) Wyjdź                               |")
+	print("---------------------------------------")
+	Snake = input("Opcja nr : ")
